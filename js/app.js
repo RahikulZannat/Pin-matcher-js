e6b04e27a12aea55091e42ec10f3c549972baf2c
Rahikul <rahikuljannat@gmail.com>
2024-02-01 21:16:47 -0500

    update

--- js/app.js
+++ js/app.js
@@ -15,3 +15,37 @@ function generatePin(){
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click',function(event){
+    const number = event.target.innerText;
+    const calc = document.getElementById('typed-numbers');
+    if(isNaN(number)){
+        if(number == 'C'){
+             calc.value = '';
+             
+        }
+    }
+    else{
+        const calcPrevious = calc.value;
+        const calcNew = calcPrevious + number;
+        calc.value = calcNew;
+    }
+   
+});
+
+document.getElementById('submit-compare').addEventListener('click', function(){
+    const displayPin = document.getElementById('display-pin');
+    const display = displayPin.value;
+
+    const typedNumber = document.getElementById('typed-numbers');
+    const typed = typedNumber.value;
+
+    // const notWorking = document.getElementById('not-working').style.display = 'none';
+    // const Working = document.getElementById('working').style.display = 'none';
+
+    if(display == typed){
+        console.log('working');
+        const working = document.getElementById('working');
+        const showing = working.innerText;
+    }
+});
